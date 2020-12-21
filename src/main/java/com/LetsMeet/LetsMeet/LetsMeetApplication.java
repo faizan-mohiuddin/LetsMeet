2dc704dbb164c5252b837b321f2369f490077fb5
faizan-mohiuddin <faizanmohiuddin@gmx.co.uk>
2020-12-21 20:06:45 +0000

    inital commit

---
+++ src/main/java/com/LetsMeet/LetsMeet/LetsMeetApplication.java
@@ -0,0 +1,13 @@
+package com.LetsMeet.LetsMeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsMeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetsMeetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LetsMeet/LetsMeet/LetsMeetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LetsMeet.LetsMeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsMeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
