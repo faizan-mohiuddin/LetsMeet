f5809200a34117d06fbe80983ac5d1999a8f9bdd
faizan-mohiuddin <faizanmohiuddin@gmx.co.uk>
2021-01-05 22:04:20 +0000

    can create users through web

--- src/main/java/com/LetsMeet/LetsMeet/WebHandler.java
+++ src/main/java/com/LetsMeet/LetsMeet/WebHandler.java
@@ -5,6 +5,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import com.LetsMeet.Models.UserData;
+import com.LetsMeet.Models.UserModel;
+
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+
+import com.LetsMeet.LetsMeet.UserManager;
 
 @Controller
 public class WebHandler {
@@ -31,6 +37,34 @@ public class WebHandler {
         model.addAttribute("eventlocation", eventlocation);
         EventHandler.createEvent(eventname, eventdesc, eventlocation);
         return "saveevent";
+    }
+
+    @GetMapping("/createuser")
+    public String createuser(Model model) {
+        model.addAttribute("user", new UserModel());
+        return "createuser";
+    }
+
+    @GetMapping("/saveuser")
+    public String saveuser(@RequestParam(name = "userfirstname") String userfirstname, @RequestParam(name = "userlastname") String userlastname, @RequestParam(name = "useremail") String useremail, @RequestParam(name = "userpassword") String userpassword, Model model) {
+        model.addAttribute("userfirstname", userfirstname);
+        model.addAttribute("userlastname", userlastname);
+        model.addAttribute("useremail", useremail);
+        model.addAttribute("userpassword", userpassword);
+
+        UserManager usrmngr = new UserManager();
+
+        byte[] theSalt = usrmngr.generateSalt();
+        byte[] theHash = usrmngr.generateHash(userpassword, theSalt);
+        UUID theUUID = usrmngr.createUserUUID(userfirstname, userlastname, useremail);
+
+        String saltString = usrmngr.toHex(theSalt);
+        String hashString = usrmngr.toHex(theHash);
+
+        UserModel usr = new UserModel();
+        usr.newUser(theUUID.toString(), userfirstname, userlastname, useremail, hashString, saltString);
+
+        return "saveuser";
     }
 
     @PostMapping("/User")
