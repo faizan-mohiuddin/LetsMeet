7d442e8375241405dcd2fae28dc65821f489864c
signal32 <55558643+signal32@users.noreply.github.com>
2021-02-02 23:33:54 +0000

    Created backend for configuration

--- src/main/java/com/LetsMeet/LetsMeet/LetsMeetApplication.java
+++ src/main/java/com/LetsMeet/LetsMeet/LetsMeetApplication.java
@@ -1,13 +1,32 @@
 package com.LetsMeet.LetsMeet;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.LetsMeet.LetsMeet.Utilities.LetsMeetConfiguration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 @SpringBootApplication
-public class LetsMeetApplication {
+public class LetsMeetApplication implements CommandLineRunner{
+
+	// Load configuration
+	@Autowired
+	LetsMeetConfiguration config;
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(LetsMeetApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception{
+		System.out.println("Using configuration:");
+		System.out.println(config.getDatabaseHost());
+		System.out.println(config.getDatabaseName());
+		System.out.println(config.getDatabaseUser());
+		System.out.println(config.getDatabasePassword());
 	}
 
 }
---
+++ src/main/java/com/LetsMeet/LetsMeet/Utilities/LetsMeetConfiguration.java
@@ -0,0 +1,44 @@
+//-----------------------------------------------------------------
+// LetsMeetConfiguration.java
+// Let's Meet 2021
+//
+// Responsible for loading and storing confirguration variables
+
+package com.LetsMeet.LetsMeet.Utilities;
+
+//-----------------------------------------------------------------
+
+import org.springframework.boot.context.properties.*;
+import org.springframework.stereotype.*;
+
+//-----------------------------------------------------------------
+
+@ConfigurationProperties(prefix="lm.config")
+@Component
+public class LetsMeetConfiguration {
+    
+    private String databaseHost;
+    private String databaseName;
+    private String databaseUser;
+    private String databasePassword;
+
+    //-----------------------------------------------------------------
+
+    public void setDatabaseHost(String x){ databaseHost = x;}
+    public String getDatabaseHost(){return databaseHost;}
+
+    public void setDatabaseName(String x){ databaseName = x;}
+    public String getDatabaseName(){return databaseName;}
+
+    public void setDatabaseUser(String x){ databaseUser = x;}
+    public String getDatabaseUser(){return databaseUser;}
+
+    public void setDatabasePassword(String x){ databasePassword = x;}
+    public String getDatabasePassword(){return databasePassword;}
+
+
+
+
+
+
+}
