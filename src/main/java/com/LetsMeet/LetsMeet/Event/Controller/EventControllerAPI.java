ee6a3a8a8c7a6264a83ac80645b80fddfaea2ae7
signal32 <55558643+signal32@users.noreply.github.com>
2021-02-08 13:18:58 +0000

    Modified

--- src/main/java/com/LetsMeet/LetsMeet/Event/Controller/EventControllerAPI.java
+++ src/main/java/com/LetsMeet/LetsMeet/Event/Controller/EventControllerAPI.java
@@ -2,6 +2,8 @@ package com.LetsMeet.LetsMeet.Event.Controller;
 
 import com.LetsMeet.LetsMeet.Event.Model.Event;
 import com.LetsMeet.LetsMeet.Event.Service.EventService;
+import com.LetsMeet.LetsMeet.User.Service.UserService;
+import com.LetsMeet.LetsMeet.User.Service.ValidationService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,26 +16,45 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class EventController {
+public class EventControllerAPI {
 
     @Autowired
     EventService eventServiceInterface;
+
+    @Autowired
+    ValidationService userValidation;
+
+    @Autowired
+    UserService userService;
+
+    // Request Mappings
+    //-----------------------------------------------------------------
 
     @RequestMapping(value = "/api/v1/event/all")
     public ResponseEntity<Object> getEvents(){
         return new ResponseEntity<>(eventServiceInterface.getEvents(),HttpStatus.OK);
     }
 
+    @RequestMapping(value = "/api/v1/event/user")
+    public ResponseEntity<Object> getUserEvents(@PathVariable("token") String token){
+        Object[] response = userValidation.verifyAPItoken(token);
+        boolean result = (boolean) response[0];
+
+        if(result){
+            return new ResponseEntity<>(userService.getSantitised(userValidation.getUserFromToken(token)),HttpStatus.OK);
+
+        }else{
+            return new ResponseEntity<>((String) response[1], HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    // Post Mappings
+    //-----------------------------------------------------------------
+
     @PostMapping(value = "/api/v1/event/{uuid}")
     public ResponseEntity<Object> updateEvent(@PathVariable("uuid") String uuid, @RequestBody Event event){
         eventServiceInterface.updateEvent(uuid, event);
         return new ResponseEntity<>("Event updated succesfully",HttpStatus.OK);
-    }
-
-    @DeleteMapping(value = "/api/v1/event/{uuid}")
-    public ResponseEntity<Object> deleteEvent(@PathVariable("uuid") String uuid){
-        eventServiceInterface.deleteEvent(uuid);
-        return new ResponseEntity<>("User deleted succesfully",HttpStatus.OK);
     }
 
     @PostMapping(value = "/api/v1/event")
@@ -43,21 +64,25 @@ public class EventController {
         return new ResponseEntity<>("User created succesfully", HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/api/v1/event/user")
-    public ResponseEntity<Object> getUserEvents(@PathVariable("uuid") String uuid){
-        return new ResponseEntity<>(eventServiceInterface.getUserEvents(uuid), HttpStatus.OK);
-    }
-
     @PostMapping (value = "/api/v1/event/user")
     public ResponseEntity<Object> addUser(@PathVariable("eventuuid") String eventUuid, @PathVariable("useruuid") String userUuid){
         eventServiceInterface.setPermissions(eventUuid, userUuid, true);
         return new ResponseEntity<>("User added succesfully", HttpStatus.OK);
     }
 
+
+     // Delete Mappings
+    //-----------------------------------------------------------------
     @DeleteMapping(value = "/api/v1/event/user")
     public ResponseEntity<Object> removeUser(@PathVariable("eventuuid") String eventUuid, @PathVariable("useruuid") String userUuid){
         eventServiceInterface.setPermissions(eventUuid, userUuid, false);
         return new ResponseEntity<>("User removed succesfully", HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/api/v1/event/{uuid}")
+    public ResponseEntity<Object> deleteEvent(@PathVariable("uuid") String uuid){
+        eventServiceInterface.deleteEvent(uuid);
+        return new ResponseEntity<>("User deleted succesfully",HttpStatus.OK);
     }
 
 
