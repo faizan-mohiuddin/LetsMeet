e7bb79b3159accb056849ea13408bd2776b674e1
signal32 <55558643+signal32@users.noreply.github.com>
2021-03-11 10:08:33 +0000

    Update Location.java

--- src/main/java/com/LetsMeet/LetsMeet/Event/Model/Variables/Location.java
+++ src/main/java/com/LetsMeet/LetsMeet/Event/Model/Variables/Location.java
@@ -4,14 +4,26 @@ import java.io.Serializable;
 
 public class Location implements Serializable {
     
+    // Is Serializable so declare version for backwards compatibility (4145802332762387198L)
+    private static final long serialVersionUID = 4145802332762387198L;
+    private String name;
     private double longitude;
     private double latitude;
     private double radius;
 
-    public Location(double latitude, double Longitude, double radius){
+    public Location(String name, double latitude, double Longitude, double radius){
+        this.name = name;
         this.latitude = latitude;
         this.longitude = Longitude;
         this.radius = radius;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
     }
 
     public double getLongitude() {
