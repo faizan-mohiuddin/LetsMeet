de248a82bcdf6f0155d3e35d792cfe4a33a7d414
signal32 <55558643+signal32@users.noreply.github.com>
2021-04-11 16:01:56 +0100

    Stability improvements

--- src/main/java/com/LetsMeet/LetsMeet/Root/Database/DatabaseConnectionPool.java
+++ src/main/java/com/LetsMeet/LetsMeet/Root/Database/DatabaseConnectionPool.java
@@ -6,6 +6,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -35,40 +36,56 @@ public class DatabaseConnectionPool {
     @Autowired
     private LetsMeetConfiguration config;
 
-    private Queue<Connection> idleConnectionPool;
-    private Queue<Connection> returnConnectionPool;
+    private Queue<Connection> readyConnectionPool;
+    private Queue<Connection> usedConnectionPool;
 
     // Thread asynchronously checks each connection object and replaces those which are no longer valid
     Thread validator = new Thread(() -> {
+
+        Queue<Connection> idleBuffer = new LinkedList<>();
+        Queue<Connection> usedBuffer;
+        Queue<Connection> swapBuffer;
+
         while (POOL_SIZE > 0){
             Instant start = Instant.now();
             try{
 
+                // Create buffers
+                usedBuffer = new LinkedList<>(usedConnectionPool);
+                usedConnectionPool.clear(); // Potential to loose some connections in time between these two methods
+
+                int failed = 0;
+
                 // Check return queue
-                Iterator<Connection> itr = returnConnectionPool.iterator();
-                while (itr.hasNext()){
-                    Connection c = itr.next();
-                    itr.remove();
-                    if (c.isValid(1)){idleConnectionPool.add(c);}
-                    else {idleConnectionPool.add(openConnection(0));}
-                }
+                for (Connection c : usedBuffer){
+                    if (c != null && c.isValid(1)){     // Add valid connections into ready queue
+                        idleBuffer.add(c);
+                    }  
+                    else failed++;                      // Discard invalid connections                        
+                } 
 
                 // Check ready queue
-                int failed = 0;
-                Iterator<Connection> iti = idleConnectionPool.iterator();
+                Iterator<Connection> iti = idleBuffer.iterator();
                 while (iti.hasNext()){
-                    Connection c = iti.next();
-                    
+                    Connection c = iti.next();      
                     if (!c.isValid(1)){
                         failed++;
                         iti.remove();
-                        idleConnectionPool.add(openConnection(0));
-
                     }
                 }
 
+                // Replace failed connection with new ones
+                for (int i = 0; i < failed; i++){
+                    idleBuffer.add(openConnection(10));
+                }
+
+                // Swap buffers
+                swapBuffer = readyConnectionPool;
+                readyConnectionPool = idleBuffer;
+                idleBuffer = swapBuffer;
+
                 // Report high failure rate
-                double failedPercentage = failed/((double) idleConnectionPool.size()+0.001);
+                double failedPercentage = failed/((double) readyConnectionPool.size()+0.001);
                 if (failedPercentage > 0.25)
                     LOGGER.warn("High proportion of failed connections detected  ({}%)", failedPercentage);
                 
@@ -88,8 +105,8 @@ public class DatabaseConnectionPool {
 
     @PostConstruct
     public void Setup() {
-        idleConnectionPool = new LinkedList<>();
-        returnConnectionPool = new LinkedList<>();
+        readyConnectionPool = new LinkedList<>();
+        usedConnectionPool = new LinkedList<>();
 
         int connectionLimit = config.getconnectionLimit();
         if (connectionLimit < 1) {
@@ -99,7 +116,7 @@ public class DatabaseConnectionPool {
 
         try {
             for (int i = 0; i < connectionLimit; i++) {
-                idleConnectionPool.add(openConnection(0));
+                readyConnectionPool.add(openConnection(0));
             }
         } catch (Exception e) {
             LOGGER.error("Database Service did not start correctly: {}", e.getMessage());
@@ -113,12 +130,21 @@ public class DatabaseConnectionPool {
      * @return a connection object from the connection pool
      */
     public Connection get() {
+        // try{
+        //     return idleConnectionPool.remove();
+        // }
+        // catch (Exception e){
+        //     LOGGER.error("Not enough connections: {}", e.getMessage());
+        //     return null;
+        // }
+
+        
         try{
         
-        return idleConnectionPool.remove();
+        return readyConnectionPool.remove();
         }
         catch (Exception e){
-            if (idleConnectionPool.peek() == null){try {
+            if (readyConnectionPool.peek() == null){try {
                 return openConnection(0);
             } catch (Exception e1) {
                 // TODO Auto-generated catch block
@@ -134,21 +160,21 @@ public class DatabaseConnectionPool {
      * @param connection
      */
     public void give(Connection connection){
-        returnConnectionPool.add(connection);
+        usedConnectionPool.add(connection);
     }
 
-    private Connection openConnection(int attempt) throws IOException, InterruptedException {
+    private Connection openConnection(int attempts) throws IOException, InterruptedException {
         try {
             return DriverManager.getConnection(config.getDatabaseHost() + "/" + config.getDatabaseName(), config.getDatabaseUser(), config.getDatabasePassword());
         } 
         catch (SQLException e) {
-            if (attempt >= 10){
-                throw new IOException("Connection was not established after "+ attempt + "attempts: " + e.getSQLState());
+            if (attempts > 0){
+                throw new IOException("Connection was not established after "+ attempts + "attempts: " + e.getSQLState());
             }
             else{
-                LOGGER.warn("Connection failed - Trying again: Attempt {}/10", attempt);
-                Thread.sleep(1000 * attempt);
-                return openConnection(++attempt);
+                LOGGER.warn("Connection failed - Trying again: Attempt {}/10", attempts);
+                Thread.sleep(1000);
+                return openConnection(--attempts);
             }
             
         }
