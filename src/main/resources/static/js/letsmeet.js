53058655c325f920e52f3c05f73cd2f84443ce00
signal32 <55558643+signal32@users.noreply.github.com>
2021-04-18 13:46:15 +0100

    Simplified and improved error handling

--- src/main/resources/static/js/letsmeet.js
+++ src/main/resources/static/js/letsmeet.js
@@ -1,10 +1,22 @@
 let player = null;
+var count = -1;
 
 
 $(document).ready(function () {
     $('.lm-egg-dangerZone').click(function () {
-
+        $(this).append(" 🎶🎵");
+        $(this).parent().animate({left: '20px'}, "slow");
+        $(this).parent().animate({left: '-20px'}, "slow");
+        $(this).parent().animate({left: '0px'}, "slow");
         player == null ? playAudio("https://ia802904.us.archive.org/24/items/TopGunThemeFilmVersionDangerZone/TopGunAnthemDangerzoneFilmIntro.mp3") : player.pause();
+    });
+
+    $('.lm-egg-dangerZone').hover(function () {
+        let move = 1;
+        $(this).parent().animate({left: `${move*count}px`}, "slow");
+        $(this).parent().animate({left: `-${move*count}px`}, "slow");
+        $(this).parent().animate({left: '0px'}, "slow");
+        count++;
     });
 
 });
