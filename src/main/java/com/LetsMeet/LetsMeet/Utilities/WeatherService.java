fec0699bd37b2147d75c63d17db220c889ccd891
Cael Milne <35670317+C-Milne@users.noreply.github.com>
2021-04-21 20:41:49 +0100

    Added future forecast

--- src/main/java/com/LetsMeet/LetsMeet/Utilities/WeatherService.java
+++ src/main/java/com/LetsMeet/LetsMeet/Utilities/WeatherService.java
@@ -1,6 +1,8 @@
 package com.LetsMeet.LetsMeet.Utilities;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -20,7 +22,7 @@ public class WeatherService {
     @Autowired
     LetsMeetConfiguration config;
 
-    public String getCurrentTemp(Double latitude, Double longitude){
+    public String getCurrentTemp(double latitude, double longitude){
         try {
             // Form url
             String requestUrl = String.format("https://api.openweathermap.org/data/2.5/onecall?lat=%f" +
@@ -52,7 +54,49 @@ public class WeatherService {
         return null;
     }
 
-    public void getWeatherForecast(){
+// Returned by below method
+//    "temp": {
+//        "day": 6.69,
+//                "min": 4.6,
+//                "max": 6.85,
+//                "night": 4.6,
+//                "eve": 5.42,
+//                "morn": 5.04
+//    },
+    public JsonObject getWeatherForecast(double latitude, double longitude, int daysInFuture){
+        if(daysInFuture > 7 || daysInFuture < 0){
+            return null;
+        }
 
+        try {
+            // Form url
+            String requestUrl = String.format("https://api.openweathermap.org/data/2.5/onecall?lat=%f" +
+                    "&lon=%f&appid=%s&exclude=minutely,hourly,current,alerts&units=metric&lang=en", latitude, longitude, config.getopenweatherApiKey());
+
+            Request request = new Request.Builder()
+                    .url(requestUrl)
+                    .build();
+
+            LOGGER.info("Weather request: " + request.toString());
+
+            // Send request
+            OkHttpClient httpClient = new OkHttpClient();
+            Response response = httpClient.newCall(request).execute();
+
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+            // Parse response
+            Gson gson = new Gson();
+            JsonObject json = gson.fromJson(response.body().string(), JsonObject.class);
+
+            JsonArray dailyTemperatures = json.get("daily").getAsJsonArray();
+            JsonObject requestedDay = dailyTemperatures.get(daysInFuture).getAsJsonObject();
+
+            return requestedDay.get("temp").getAsJsonObject();
+
+        }catch(Exception e){
+            LOGGER.warn("Error accessing current weather: {}", e.getMessage());
+        }
+        return null;
     }
 }
