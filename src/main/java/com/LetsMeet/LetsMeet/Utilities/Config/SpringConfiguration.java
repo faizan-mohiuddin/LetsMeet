44c999099c1d1768b258768936fc57bbcff9fe69
signal32 <55558643+signal32@users.noreply.github.com>
2021-04-21 11:13:45 +0100

    Localisation support
    Obviously requires pages to be translated, but the support is there.

--- src/main/java/com/LetsMeet/LetsMeet/Utilities/Config/SpringConfiguration.java
+++ src/main/java/com/LetsMeet/LetsMeet/Utilities/Config/SpringConfiguration.java
@@ -8,14 +8,20 @@ package com.LetsMeet.LetsMeet.Utilities.Config;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Locale;
 
 import com.LetsMeet.LetsMeet.Utilities.LetsMeetConfiguration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
- 
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
 @Configuration
 public class SpringConfiguration implements WebMvcConfigurer {
 
@@ -35,5 +41,24 @@ public class SpringConfiguration implements WebMvcConfigurer {
          
         if (dirName.startsWith("../")) dirName = dirName.replace("../", "");
         registry.addResourceHandler("/" + "media" + "/**").addResourceLocations("/" + uploadPath +"/","file:///"+ uploadPath + "/");
+    }
+
+    /* Resolve users locale */
+
+    @Bean
+    public LocaleResolver localeResolver(){
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US);
+        return  localeResolver;
+    }
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor(){
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
     }
 }
